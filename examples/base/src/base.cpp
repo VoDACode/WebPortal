c6f64a2f5d4a67e28f2627b279319ea2bac50565
VoDACode <dimavovk201112@gmail.com>
2023-10-16 16:27:41 +0300

    🔧Bug fix

--- examples/base/src/base.cpp
+++ examples/base/src/base.cpp
@@ -26,7 +26,7 @@ HtmlNode *buildPage();
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   webPortal.setPageTitle("Base example");
   webPortal.setTitle("WebPortal - Base example");
 
